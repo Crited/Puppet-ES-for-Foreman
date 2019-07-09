1a93c0bcaf87fb5746bb0e6c5bb973195cc1cc66
Crited <45196568+Crited@users.noreply.github.com>
2019-07-09 12:19:47 +0200

    Update init.pp

--- modules/foreman_elasticsearch/manifests/init.pp
+++ modules/foreman_elasticsearch/manifests/init.pp
@@ -6,21 +6,12 @@ $esport      = '9200',
 $rsyslogpath = '/etc/rsyslog.d',
 )
 {
-  include ::foreman_elasticsearch::install
-  include ::foreman_elasticsearch::vcsrepo
-  include ::foreman_elasticsearch::template
-  include ::foreman_elasticsearch::selinux
-  include ::foreman_elasticsearch::rsyslog
-  include ::foreman_elasticsearch::rails
-  include ::foreman_elasticsearch::candlepin
-  include ::foreman_elasticsearch::logrotate
-
-  Class['::foreman_elasticsearch::install']   -> 
-  Class['::foreman_elasticsearch::vcsrepo']   ->
-  Class['::foreman_elasticsearch::template']  ->
-  Class['::foreman_elasticsearch::selinux']   ->
-  Class['::foreman_elasticsearch::rsyslog']   ->
-  Class['::foreman_elasticsearch::rails']     ->
-  Class['::foreman_elasticsearch::candlepin'] ->
-  Class['::foreman_elasticsearch::logrotate']
+  class{'foreman_elasticsearch::install': }   -> 
+  class{'foreman_elasticsearch::vcsrepo': }   ->
+  class{'foreman_elasticsearch::template': }  ->
+  class{'foreman_elasticsearch::selinux': }   ->
+  class{'foreman_elasticsearch::rsyslog': }   ->
+  class{'foreman_elasticsearch::rails': }     ->
+  class{'foreman_elasticsearch::candlepin': } ->
+  class{'foreman_elasticsearch::logrotate': }
 }
